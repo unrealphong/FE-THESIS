8ef6a52d40a7fb165a168e415872888287935a0a
quan1234 <quanvdph19781@fpt.edu.vn>
2024-05-24 07:37:16 +0700

    create page listproduct

---
+++ src/pages/ListProduct.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ListProduct = (props: Props) => {
+  return (
+    <div><h1>Danh sách sản phẩm</h1></div>
+  )
+}
+
+export default ListProduct
