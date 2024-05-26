fe8f0440290de1522754c4a967bdc9501d163a46
unrealphong <hongphongle115@gmail.com>
2024-05-27 02:54:51 +0700

    hotfix(ui): error displaying font color on chrome

--- src/layouts/un-auth/un-auth-layout.tsx
+++ src/layouts/un-auth/un-auth-layout.tsx
@@ -1,12 +1,16 @@
 import Footer from "@/Components/Footer/Footer"
 import Header from "@/Components/Header/Header"
 import { Outlet } from "react-router-dom"
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/scrollbar"
 
 function UnAuthLayout() {
   return (
     <>
       <Header />
-      <div className="h-[3000px]">
+      <div className="container pt-2 pb-10">
         <Outlet />
       </div>
       <Footer />
