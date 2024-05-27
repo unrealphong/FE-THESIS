e7f873859ac2e640897ce16b17b7a6a4936c9c8f
Phát <phatvdph20067@fpt.edu.vn>
2024-05-27 07:36:23 +0700

    UI ListProduct

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
-import './index.css'
+import "./index.css"
 import { createBrowserRouter, RouteObject, RouterProvider } from "react-router-dom"
 import { AppProvider } from "./AppProvider.tsx"
 import { UnAuthRouter } from "./routes/root-router.tsx"
