6ac4b9ca0b5028818b6c174990e8791d718084d1
Phát <phatvdph20067@fpt.edu.vn>
2024-05-27 20:44:32 +0700

    UI Cart

--- src/routes/un-auth-router.tsx
+++ src/routes/un-auth-router.tsx
@@ -2,7 +2,7 @@ import HomePage from "@/pages/HomePage"
 import LoginPage from "@/pages/LoginPage/LoginPage"
 import RegisterPage from "@/pages/RegisterPage/RegisterPage"
 import { pathName } from "@/routes/path-name"
-import Cart from '../Components/Cart/Cart';
+import Cart from "../Components/Cart/Cart"
 
 export const UnAuthRouter = [
   {
