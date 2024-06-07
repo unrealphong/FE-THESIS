3910fd0c6f493826328e06f78d7e88759244ddd3
Phát <phatvdph20067@fpt.edu.vn>
2024-06-07 14:03:28 +0700

    hiển thị danh mục sản phẩm phat

--- src/pages/ListProduct/CategoryInListProduct.tsx
+++ src/pages/ListProduct/CategoryInListProduct.tsx
@@ -1,16 +1,16 @@
-import { Category } from '@/@types/category'
-import { Checkbox } from 'antd'
+import { Category } from "@/@types/category"
+import { Checkbox } from "antd"
 interface CategoryInListProductProps {
-    data: Category;
+  data: Category
 }
 const CategoryInListProduct: React.FC<CategoryInListProductProps> = ({ data }) => {
-    return (
-        <>
-            <div>
-                <Checkbox value={data?._id}>{data?.title}</Checkbox>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <Checkbox value={data?._id}>{data?.title}</Checkbox>
+      </div>
+    </>
+  )
 }
 
 export default CategoryInListProduct
